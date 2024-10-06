90353d6346bf7e2992700df0f128f579f43011b3
CookieMan <sm7072@srmist.edu.in>
2024-10-06 20:02:42 +0530

    model.py

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score, classification_report
 
-#Please update the path of the dataset according to where you save them 
+#Please update the path of the dataset according to where you save them ( Probably already know it but just in case :) )
 train_data = pd.read_csv('C:\Python\Debug\BBC News Train.csv')
 test_data = pd.read_csv('C:\Python\Debug\BBC News Test.csv')
 
